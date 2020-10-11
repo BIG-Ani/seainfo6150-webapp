adf8b25981bf8ee314f8c89c29bd7fab4fa14538
Leichen Zhou <leichenzhou@Leichens-MacBook-Pro.local>
2020-10-11 13:33:43 -0700

    js assignment

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import DynamicArticle from "./DynamicArticle/DynamicArticle";
 import { isEmpty } from "lodash";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState({});
+  const [fetchedData, setFetchedData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       // put data fetching code here!
+      const response = await fetch(
+        "http://demo1390455.mockable.io/articles"
+      );
+      const responseJson = await response.json();
+
+      console.log("GET RESP", responseJson);
+      setFetchedData(responseJson);
     };
 
     if (isEmpty(fetchedData)) {
@@ -17,13 +24,31 @@ function App() {
     }
   }, [fetchedData]);
 
+
   return isEmpty(fetchedData) ? null : (
     <div className="App">
       <Switch>
         <Route>
-          <DynamicArticle article={Object.values(fetchedData)[1]} />
+            <article>
+              <header>
+                <h1>{Object.values(fetchedData)[1].title}</h1>
+                
+                <address>
+                  by {Object.values(fetchedData)[1].author} (
+                  <a href={Object.values(fetchedData)[1].authorEmail}>{Object.values(fetchedData)[1].authorEmail}</a>)
+                  <br />
+                </address>
+
+                <time datetime={Object.values(fetchedData)[1].timeStamp}>{Object.values(fetchedData)[1].displayDate} </time>
+              </header>
+
+              <div dangerouslySetInnerHTML = {{ __html: Object.values(fetchedData)[1].text }} />
+
+            </article>
+            
         </Route>
       </Switch>
+
     </div>
   );
 }
